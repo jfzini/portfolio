72bc03110ee6de95bfe2bb62994d05e7e59977b0
João Felipe Zini <joaofelipezini@gmail.com>
2023-08-09 20:24:23 -0300

    adding stack names on hover

--- src/hero/StackImg.jsx
+++ src/hero/StackImg.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-export default function StackImg({src, alt}) {
+export default function StackImg({ src, alt }) {
   return (
-    <img src={src} alt={alt} className="stack__img"/>
-  )
+    <>
+      <div className='stack__container'>
+        <img src={src} alt={alt} className="stack__img" />
+        <div className="stack__title">{alt}</div>
+      </div>
+    </>
+  );
 }
