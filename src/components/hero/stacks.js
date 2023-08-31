9ac30c43f954f947bccfe5bb7e192f08245485ef
João Felipe Zini <joaofelipezini@gmail.com>
2023-08-31 11:19:51 -0300

    feat: adding project and stack

--- src/components/hero/stacks.js
+++ src/components/hero/stacks.js
@@ -1,113 +1,118 @@
-import reactIcon from '../../images/stacks/react.png'
-import htmlIcon from '../../images/stacks/html.png'
-import cssIcon from '../../images/stacks/css.png'
-import jsIcon from '../../images/stacks/javascript.png'
-import nodeIcon from '../../images/stacks/nodejs.png'
-import gitIcon from '../../images/stacks/git.png'
-import rtlIcon from '../../images/stacks/rtl.png'
-import jestIcon from '../../images/stacks/jest.png'
-import tsIcon from '../../images/stacks/typescript.png'
-import angularIcon from '../../images/stacks/angular.png'
-import reduxIcon from '../../images/stacks/redux.png'
-import bootstrapIcon from '../../images/stacks/bootstrap.png'
-import muiIcon from '../../images/stacks/mui.png'
-import expressIcon from '../../images/stacks/express.png'
-import dockerIcon from '../../images/stacks/docker.png'
-import mysqlIcon from '../../images/stacks/mysql.png'
-import mochaIcon from '../../images/stacks/mocha.png'
-import chaiIcon from '../../images/stacks/chai.png'
-import sinonIcon from '../../images/stacks/sinon.png'
-import sassIcon from '../../images/stacks/sass.png'
-import viteIcon from '../../images/stacks/vite.png'
-import i18nIcon from '../../images/stacks/i18next.png'
+import reactIcon from '../../images/stacks/react.png';
+import htmlIcon from '../../images/stacks/html.png';
+import cssIcon from '../../images/stacks/css.png';
+import jsIcon from '../../images/stacks/javascript.png';
+import nodeIcon from '../../images/stacks/nodejs.png';
+import gitIcon from '../../images/stacks/git.png';
+import rtlIcon from '../../images/stacks/rtl.png';
+import jestIcon from '../../images/stacks/jest.png';
+import tsIcon from '../../images/stacks/typescript.png';
+import angularIcon from '../../images/stacks/angular.png';
+import reduxIcon from '../../images/stacks/redux.png';
+import bootstrapIcon from '../../images/stacks/bootstrap.png';
+import muiIcon from '../../images/stacks/mui.png';
+import expressIcon from '../../images/stacks/express.png';
+import dockerIcon from '../../images/stacks/docker.png';
+import mysqlIcon from '../../images/stacks/mysql.png';
+import mochaIcon from '../../images/stacks/mocha.png';
+import chaiIcon from '../../images/stacks/chai.png';
+import sinonIcon from '../../images/stacks/sinon.png';
+import sassIcon from '../../images/stacks/sass.png';
+import viteIcon from '../../images/stacks/vite.png';
+import i18nIcon from '../../images/stacks/i18next.png';
+import sequelizeIcon from '../../images/stacks/sequelize.png';
 
 export const stacks = [
   {
     name: 'HTML',
-    icon: htmlIcon
+    icon: htmlIcon,
   },
   {
     name: 'CSS',
-    icon: cssIcon
+    icon: cssIcon,
   },
   {
     name: 'SASS',
-    icon: sassIcon
+    icon: sassIcon,
   },
   {
     name: 'JavaScript',
-    icon: jsIcon
+    icon: jsIcon,
   },
   {
     name: 'TypeScript',
-    icon: tsIcon
+    icon: tsIcon,
   },
   {
     name: 'Git',
-    icon: gitIcon
+    icon: gitIcon,
   },
   {
     name: 'Vite',
-    icon: viteIcon
+    icon: viteIcon,
   },
   {
     name: 'React',
-    icon: reactIcon
+    icon: reactIcon,
   },
   {
     name: 'Redux',
-    icon: reduxIcon
+    icon: reduxIcon,
   },
   {
     name: 'Angular',
-    icon: angularIcon
+    icon: angularIcon,
   },
   {
     name: 'Bootstrap',
-    icon: bootstrapIcon
+    icon: bootstrapIcon,
   },
   {
     name: 'Material UI',
-    icon: muiIcon
+    icon: muiIcon,
   },
   {
     name: 'i18next',
-    icon: i18nIcon
+    icon: i18nIcon,
   },
   {
     name: 'Node.js',
-    icon: nodeIcon
+    icon: nodeIcon,
   },
   {
     name: 'Express.js',
-    icon: expressIcon
+    icon: expressIcon,
   },
   {
     name: 'Docker',
-    icon: dockerIcon
+    icon: dockerIcon,
   },
   {
     name: 'MySQL',
-    icon: mysqlIcon
+    icon: mysqlIcon,
+  },
+  {
+    name: 'Sequelize',
+    icon: sequelizeIcon,
   },
   {
     name: 'Jest',
-    icon: jestIcon
+    icon: jestIcon,
   },
   {
     name: 'RTL',
-    icon: rtlIcon
+    icon: rtlIcon,
   },
   {
     name: 'Mocha',
-    icon: mochaIcon
+    icon: mochaIcon,
   },
   {
     name: 'Chai',
-    icon: chaiIcon
+    icon: chaiIcon,
   },
   {
     name: 'Sinon',
-    icon: sinonIcon
+    icon: sinonIcon,
   },
-]
+];
--- src/components/projects/projectsData.js
+++ src/components/projects/projectsData.js
@@ -1,6 +1,7 @@
 import recipesHero from '../../images/projects/recipes-hero.png';
 import snippetsHero from '../../images/projects/snippets-hero.png';
-import managerHero from '../../images/projects/manager-hero.png';
+import talkerManagerHero from '../../images/projects/talker-manager-hero.png';
+import storeManagerHero from '../../images/projects/store-manager-hero.png';
 import dragHero from '../../images/projects/drag-hero.png';
 // import solarHero from '../../images/projects/solar-system-hero.png';
 import walletHero from '../../images/projects/wallet-hero.png';
@@ -8,35 +9,48 @@ import walletHero from '../../images/projects/wallet-hero.png';
 export const projects = [
   {
     title: 'Recipes App',
-    description: 'This is an app that shows meals and drinks recipes, allow the user to keep track of the steps and ingredients, and also to favorite, filter and search for recipes. It was made with React and ContextAPI to manage the state, and followed a mobile-first approach.',
+    description:
+      'This is an app that shows meals and drinks recipes, allow the user to keep track of the steps and ingredients, and also to favorite, filter and search for recipes. It was made with React and ContextAPI to manage the state, and followed a mobile-first approach.',
     gitHubUrl: 'https://github.com/jfzini/recipes-app',
     deployedUrl: 'https://good-cooking-app.vercel.app/',
     videoUrl: '',
-    imgSrc: recipesHero
+    imgSrc: recipesHero,
   },
   {
     title: 'Music Snippets',
-    description: 'Fully responsive app that allows the user to search for artists and songs, to save them as favorites and edit their profile in the app.',
+    description:
+      'Fully responsive app that allows the user to search for artists and songs, to save them as favorites and edit their profile in the app.',
     gitHubUrl: 'https://github.com/jfzini/Zini-Tunes',
     deployedUrl: 'https://music-snippets.vercel.app/',
     videoUrl: '',
-    imgSrc: snippetsHero
+    imgSrc: snippetsHero,
   },
   {
     title: 'Trip Wallet',
-    description: "This project is a simulation of a wallet, where you can add expenses, convert the spent currencies to Brazilian Real, and view a table with the total expenses. The development was carried out using Redux, React, React Router, and Middlewares for handling asynchronous actions. The tests were developed with React Testing Library.",
+    description:
+      'This project is a simulation of a wallet, where you can add expenses, convert the spent currencies to Brazilian Real, and view a table with the total expenses. The development was carried out using Redux, React, React Router, and Middlewares for handling asynchronous actions. The tests were developed with React Testing Library.',
     gitHubUrl: 'https://github.com/jfzini/trip-wallet',
     deployedUrl: 'https://trip-wallet-project.vercel.app/',
     videoUrl: '',
-    imgSrc: walletHero
+    imgSrc: walletHero,
   },
   {
     title: 'Talker Manager',
-    description: 'This is a back-end project that allows the user to create, edit and delete (CRUD) talkers for an event. It was made with Node.js, Express, Docker and MySQL. It follows a layered architecture, with a service layer, a controller layer and a model layer.',
+    description:
+      'This is a back-end project that allows the user to create, edit and delete (CRUD) talkers for an event. It was made with Node.js, Express, Docker and MySQL. It follows a layered architecture, with a service layer, a controller layer and a model layer.',
     gitHubUrl: 'https://github.com/jfzini/talker-manager-express-api',
     deployedUrl: '',
     videoUrl: '',
-    imgSrc: managerHero
+    imgSrc: talkerManagerHero,
+  },
+  {
+    title: 'Store Manager',
+    description:
+      'This is an API developed using Docker, Node.js, Express and MySQL but the true focus of the project is the testing. It has unit and integration tests, with 100% coverage and quality assured by mutation tests with Stryker.',
+    gitHubUrl: 'https://github.com/jfzini/store-manager-API',
+    deployedUrl: 'https://documenter.getpostman.com/view/29177531/2s9Y5R3SWC',
+    videoUrl: '',
+    imgSrc: storeManagerHero,
   },
   {
     title: 'Top Trumps Drag',
@@ -44,7 +58,7 @@ export const projects = [
     gitHubUrl: 'https://github.com/jfzini/top_trumps_drag',
     deployedUrl: 'https://top-trumps-drag.vercel.app/',
     videoUrl: 'https://www.youtube.com/embed/JSxDvSl0ceA',
-    imgSrc: dragHero
+    imgSrc: dragHero,
   },
   // {
   //   title: 'Solar System',
@@ -54,4 +68,4 @@ export const projects = [
   //   videoUrl: 'https://www.youtube.com/embed/4GuyL_BArd8',
   //   imgSrc: solarHero
   // },
-]
+];
